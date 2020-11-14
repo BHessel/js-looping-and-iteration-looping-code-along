86c45c722776162bce32792839271e81cbac3452
BHessel <hesselfitness@gmail.com>
2020-11-14 18:43:22 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+function writeCards(name, event){
+    let new_array = [];
+    for (let i = 0; i < name.length; i++){
+        new_array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return new_array;
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--);
+    }
+}
